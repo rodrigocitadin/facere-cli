647de8a5e23539803ad6bcbc1899b373dfaf06dc
rodrigocitadin <citadinrodrigo@gmail.com>
2025-02-25 16:44:32 -0300

    task base structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Task {
+    id: u32,
+    description: String,
+    date: String, // YYYY-MM-DD
+    completed: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
 }
